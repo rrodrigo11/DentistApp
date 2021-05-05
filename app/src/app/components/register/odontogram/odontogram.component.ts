c38cab3acc3e957381cbb763acb320cb8b4d8a6e
Ivan Iturbe <ivan.iturbeo@hotmail.com>
2021-05-04 22:11:57 -0500

    Odontograma avance

--- app/src/app/components/register/odontogram/odontogram.component.ts
+++ app/src/app/components/register/odontogram/odontogram.component.ts
@@ -1,3 +1,4 @@
+import { stringify } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,10 +10,37 @@ export class OdontogramRegisterComponent implements OnInit {
 
   imageSrc = '../../../assets/odon.jpeg'  
   imageAlt = 'iPhone'
-
+  dientesview:string = ''
+  dientework:string = ''
+  dienteview:any
+  dientes:any = {
+    "18":"",
+    "17":"",
+    "16":"",
+    "15":"",
+    "14":"",
+    "13":"",
+    "12":"",
+    "11":"",
+    "21":"",
+ 
+  }
+  vistas:boolean[]= [true,false,false,false,false,false,false,false]
   constructor() { }
+  isVisible:boolean = false;
 
   ngOnInit(): void {
+  }
+  clickarea(diente:string){
+    this.dientework =  this.dientes[diente]
+    this.dientesview = diente
+    this.dienteview =  `<h1>`+diente+`</h1>`
+    this.dientes[this.dientesview] = this.dientework;
+    console.log( this.dientes[this.dientesview]);
+    this.dientes[+diente]
+    console.log( this.dientes);
+
+
   }
 
 }
